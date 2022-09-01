8c0bb98b85f8a8af392d82ff2eed7638086673d1
Augusto Cardinale <94425313+AugustoCardinale@users.noreply.github.com>
2022-09-01 19:16:13 -0300

    Add files via upload

---
+++ CoderHouse/sweetalert.js
@@ -0,0 +1,9 @@
+Swal.fire({
+    title: 'Bienvenido!',
+    text: 'sos mayor de edad?',
+    icon: 'question',
+    confirmButtonText: 'Si',
+    cancelButtonText: 'No',
+    showCancelButton: true,
+    showCloseButton: true
+  })
